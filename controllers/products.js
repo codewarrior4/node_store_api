39214a43d5ce70e8652d525d465d9637476f8fd0
codewarrior4 <mayowatope119@gmail.com>
2023-04-15 15:24:57 +0100

    regex pattern

--- controllers/products.js
+++ controllers/products.js
@@ -1,18 +1,24 @@
 
 const Products = require('../models/product')
 const getAllProductsStatic = async(req, res) =>{
-    const products = await Products.find({})
+    const search = 'ba'
+    const products = await Products.find({
+        name: {$regex: search, $options: 'i'}
+    })
     res.status(200).json({success: true, products})
 }
 
 const getAllProducts = async(req, res) =>{
-    const {featured,company} = req.query
+    const {featured,company,name} = req.query
     const queryObject = {}
     if(featured){
         queryObject.featured = featured === 'true' ? true : false
     }
     if(company){
         queryObject.company = company
+    }
+    if(name){
+        queryObject.name = {$regex: name, $options: 'i'}
     }
     const products = await Products.find(queryObject)
     res.status(200).json({success:true,products,count:products.length})
